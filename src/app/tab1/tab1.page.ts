71823f3a856e20e031a87765e2fdacde1a779ed7
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-04-20 14:24:22 -0300

    Primeiro commit.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, ReactiveFormsModule],
 })
 export class Tab1Page {
-  constructor() {}
+  formGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = formBuilder.group({
+      motivo: ['',[Validators.required]],
+      valor: [0.0,[Validators.required]],
+      tipo: ['',[Validators.required]],
+      data: [new Date(),[Validators.required]]
+    })
+  }
 }
